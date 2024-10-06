c58f3b8ddac3040184a3b06c80689328792b8235
Esraa Abdellatif <esraa.abd.a99@gmail.com>
2024-10-06 19:58:59 +0300

    refactoring Project Structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter_animation/TweenAnimationBuilder/tween_animation_builder.dart';
+import 'package:flutter_animation/Implicit%20Animations/TweenAnimationBuilder/tween_animation_builder.dart';
 void main() {
   runApp((DevicePreview(
     enabled: true,
