ab589005f4c3e3bbb859eea2bcb0c7ad58a2be50
Esraa Abdellatif <esraa.abd.a99@gmail.com>
2024-10-07 09:40:49 +0300

    Animated Builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter_animation/Implicit%20Animations/TweenAnimationBuilder/tween_animation_builder.dart';
+import 'package:flutter_animation/Explicit%20Animations/AnimatedBuilder/animated_builder.dart';
 void main() {
   runApp((DevicePreview(
     enabled: true,
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const TweenAnimationBuilderWidget(),
+      home: const AnimatedBuilderWidget(),
     );
   }
 }
