841d672f748a7053c37495de1801520502edb19e
Esraa Abdellatif <esraa.abd.a99@gmail.com>
2024-10-07 11:28:13 +0300

    Explicit Animations Exmple

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter_animation/Explicit%20Animations/ExplicitAnimationAndTransform/animation_transform.dart';
+import 'package:flutter_animation/Explicit%20Animations/Explicit%20Animations%20Exmple/explicit_animation.dart';
 void main() {
   runApp((DevicePreview(
     enabled: true,
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const AnimationAndTransformWidget(),
+      home: const ExplicitAnimationsWidget(),
     );
   }
 }
